77c1a0720ce66d3c7e2f292d6cd11afe4c61b377
unknown <sudhanplayz@gmail.com>
2022-12-10 21:17:08 +0530

    js -> jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>BitElite</title>
         <meta name="BitElite" content="We love to BUIDL!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className='mx-auto text-5xl'>
-          BitElite
-        </h1>
-      </main>
+      <section>
+
+      </section>
     </div>
   )
 }
